1f64f42c42c219869a2d6760766572e5c647c78e
Hrosas56789 <42222210+Hrosas56789@users.noreply.github.com>
2018-08-14 14:59:43 -0700

    Add files via upload

---
+++ 180814.py
@@ -0,0 +1,30 @@
+'''
+Hector Rosas
+2018 - 08 - 14
+
+Working with Strings
+Resources : Corey Schafer : Youtube working with textual data
+'''
+
+# a = "Salutations"
+# b = "World"
+#
+# print(a, b)
+# print(a+b)
+# print(a + " " + b)
+
+# a = "12"
+# b = "13"
+#
+# print(a + b)
+
+# a = input("the 1st number:")
+# b = input("the 2nd number:")
+# print(a + b)
+#
+# a = int(input("the 1st number:"))
+# b = int(input("the 2nd number:"))
+# print(a + b)
+
+c = max(-1, 0, -9, 12, 3.56, 200)
+print(c)
